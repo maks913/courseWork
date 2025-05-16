a20103ef6cd5360da4e0516edf0e9256cd275062
Maks Balinskij <maksbalinskij@gmail.com>
2025-05-16 14:01:48 +0300

    Editable Drinks

--- src/main/java/main/controller/drinkTableController.java
+++ src/main/java/main/controller/drinkTableController.java
@@ -5,8 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.converter.DoubleStringConverter;
 import main.logic.Drink;
 import main.service.DrinkService;
+
 import java.util.List;
 
 public class drinkTableController {
@@ -19,10 +22,10 @@ public class drinkTableController {
     private TableColumn<Drink, Integer> ID;
 
     @FXML
-    private TableColumn<Drink, String> NAME,AVAILABLE,SERVING,TYPE;
+    private TableColumn<Drink, String> NAME, AVAILABLE, SERVING, TYPE;
 
     @FXML
-    private TableColumn<Drink, Double> PRICE,RATING;
+    private TableColumn<Drink, Double> PRICE, RATING;
 
     public void updateTable(List<Drink> drinks) {
         drinkTable.setItems(FXCollections.observableArrayList(drinks));
@@ -30,13 +33,35 @@ public class drinkTableController {
 
     @FXML
     void initialize() {
+        drinkTable.setEditable(true);
+
         ID.setCellValueFactory(new PropertyValueFactory<>("id"));
+
         NAME.setCellValueFactory(new PropertyValueFactory<>("name"));
+        NAME.setCellFactory(TextFieldTableCell.forTableColumn());
+        NAME.setOnEditCommit(event -> event.getRowValue().setName(event.getNewValue()));
+
         PRICE.setCellValueFactory(new PropertyValueFactory<>("price"));
+        PRICE.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
+        PRICE.setOnEditCommit(event -> event.getRowValue().setPrice(event.getNewValue()));
+
         TYPE.setCellValueFactory(new PropertyValueFactory<>("type"));
+        TYPE.setCellFactory(TextFieldTableCell.forTableColumn());
+        TYPE.setOnEditCommit(event -> event.getRowValue().setType(event.getNewValue()));
+
         AVAILABLE.setCellValueFactory(new PropertyValueFactory<>("available"));
+        AVAILABLE.setCellFactory(TextFieldTableCell.forTableColumn());
+        AVAILABLE.setOnEditCommit(event -> event.getRowValue().setAvailable(event.getNewValue()));
+
         SERVING.setCellValueFactory(new PropertyValueFactory<>("servingTemperature"));
+        SERVING.setCellFactory(TextFieldTableCell.forTableColumn());
+        SERVING.setOnEditCommit(event -> event.getRowValue().setServingTemperature(event.getNewValue()));
+
         RATING.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        RATING.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
+        RATING.setOnEditCommit(event -> event.getRowValue().setRating(event.getNewValue()));
+
         updateTable(service.getAllDrinks());
+
     }
 }
